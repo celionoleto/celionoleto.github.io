97a0100dbc8943e27a20ca2df7d4973f1b001481
celionoleto <celiobyte@hotmail.com>
2023-04-08 23:50:11 -0300

    att nova versao

---
+++ menu.js
@@ -0,0 +1,24 @@
+let show = true; 
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+const listClick = document.querySelector(".list")
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
+
+listClick.addEventListener("click", () => {
+    
+
+    menuSection.classList.toggle("on")
+    document.body.style.overflow = show ? "hidden" : "initial"
+    window.location.reload(true);
+   
+}
+
+)
